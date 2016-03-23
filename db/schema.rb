d30bed1aa2c10e843e3118449145f1680e7fcf24
Meg <forlornlawngnome@gmail.com>
2016-03-22 20:49:43 -0400

    run db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,95 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322222551) do
+ActiveRecord::Schema.define(version: 20160323004922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "awards", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "awards_competitions", force: :cascade do |t|
+    t.integer  "competition_id"
+    t.integer  "award_id"
+    t.text     "blurb"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "awards_competitions", ["award_id"], name: "index_awards_competitions_on_award_id", using: :btree
+  add_index "awards_competitions", ["competition_id"], name: "index_awards_competitions_on_competition_id", using: :btree
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "competitions", force: :cascade do |t|
+    t.string   "name"
+    t.date     "date"
+    t.integer  "year_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "competitions", ["year_id"], name: "index_competitions_on_year_id", using: :btree
+
+  create_table "document_links", force: :cascade do |t|
+    t.integer  "document_id"
+    t.text     "doc_link"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "document_links", ["document_id"], name: "index_document_links_on_document_id", using: :btree
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "name"
+    t.date     "date_published"
+    t.text     "about"
+    t.integer  "category_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "documents", ["category_id"], name: "index_documents_on_category_id", using: :btree
+
+  create_table "documents_people", force: :cascade do |t|
+    t.integer  "person_id"
+    t.integer  "document_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "documents_people", ["document_id"], name: "index_documents_people_on_document_id", using: :btree
+  add_index "documents_people", ["person_id"], name: "index_documents_people_on_person_id", using: :btree
+
+  create_table "game_videos", force: :cascade do |t|
+    t.integer  "game_id"
+    t.string   "title"
+    t.text     "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "game_videos", ["game_id"], name: "index_game_videos_on_game_id", using: :btree
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "year_id"
+    t.string   "name"
+    t.text     "strategy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "games", ["year_id"], name: "index_games_on_year_id", using: :btree
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -28,10 +113,45 @@ ActiveRecord::Schema.define(version: 20160322222551) do
 
   add_index "people", ["role_id"], name: "index_people_on_role_id", using: :btree
 
+  create_table "robots", force: :cascade do |t|
+    t.string   "name"
+    t.text     "descripton"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsor_levels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsor_years", force: :cascade do |t|
+    t.integer  "sponsor_id"
+    t.integer  "year_id"
+    t.integer  "sponsor_level_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "sponsor_years", ["sponsor_id"], name: "index_sponsor_years_on_sponsor_id", using: :btree
+  add_index "sponsor_years", ["sponsor_level_id"], name: "index_sponsor_years_on_sponsor_level_id", using: :btree
+  add_index "sponsor_years", ["year_id"], name: "index_sponsor_years_on_year_id", using: :btree
+
+  create_table "sponsors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo_link"
+    t.text     "sponsor_url"
+    t.boolean  "active"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "years", force: :cascade do |t|
@@ -41,5 +161,17 @@ ActiveRecord::Schema.define(version: 20160322222551) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "awards_competitions", "awards"
+  add_foreign_key "awards_competitions", "competitions"
+  add_foreign_key "competitions", "years"
+  add_foreign_key "document_links", "documents"
+  add_foreign_key "documents", "categories"
+  add_foreign_key "documents_people", "documents"
+  add_foreign_key "documents_people", "people"
+  add_foreign_key "game_videos", "games"
+  add_foreign_key "games", "years"
   add_foreign_key "people", "roles"
+  add_foreign_key "sponsor_years", "sponsor_levels"
+  add_foreign_key "sponsor_years", "sponsors"
+  add_foreign_key "sponsor_years", "years"
 end
