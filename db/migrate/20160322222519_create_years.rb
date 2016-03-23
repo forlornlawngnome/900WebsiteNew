7d7b4e18c12900644641d902cd45020f0c32f918
Meg <forlornlawngnome@gmail.com>
2016-03-22 21:50:26 -0400

    change year end to a date

--- db/migrate/20160322222519_create_years.rb
+++ db/migrate/20160322222519_create_years.rb
@@ -2,7 +2,7 @@ class CreateYears < ActiveRecord::Migration
   def change
     create_table :years do |t|
       t.date :start_date
-      t.string :end_date
+      t.date :end_date
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -166,15 +166,6 @@ ActiveRecord::Schema.define(version: 20160323012409) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "social_media", force: :cascade do |t|
-    t.string   "name"
-    t.string   "icon"
-    t.text     "url"
-    t.integer  "order"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "social_media_links", force: :cascade do |t|
     t.string   "name"
     t.string   "icon"
@@ -213,7 +204,7 @@ ActiveRecord::Schema.define(version: 20160323012409) do
 
   create_table "years", force: :cascade do |t|
     t.date     "start_date"
-    t.string   "end_date"
+    t.date     "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
