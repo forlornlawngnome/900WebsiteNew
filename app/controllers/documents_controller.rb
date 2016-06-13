be18f97af57af73da1780d0556bb92b1f56c80b9
Meg <forlornlawngnome@gmail.com>
2016-06-12 23:53:42 -0400

    create new document links with document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -73,6 +73,6 @@ class DocumentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def document_params
-      params.require(:document).permit(:name, :date_published, :about, :category_id)
+      params.require(:document).permit(:name, :date_published, :about, :category_id, document_links_attributes: [:id, :document_id, :doc_link, :name, :_destroy])
     end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,7 @@ class Document < ActiveRecord::Base
   belongs_to :category
   has_many :document_links
   has_and_belongs_to_many :people
+  accepts_nested_attributes_for :document_links, allow_destroy: true, reject_if: :all_blank
   
   validates_presence_of :name, :date_published, :about, :category_id
 end
