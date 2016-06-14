38ed4e2047d5cde26609eb6823d05bfb4b509a60
Meg <forlornlawngnome@gmail.com>
2016-06-14 19:42:14 -0400

    add users in the document form

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,6 +3,7 @@ class Document < ActiveRecord::Base
   has_many :document_links
   has_and_belongs_to_many :people
   accepts_nested_attributes_for :document_links, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :people
   
   validates_presence_of :name, :date_published, :about, :category_id
 end
