d9d7692c9da7e04bdd610ec6ca2ac49f5deacd74
Meg <forlornlawngnome@gmail.com>
2016-06-25 11:30:15 -0400

    custom page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def mentor_email_link
     "<a href=\"mailto:mentors@team900.org\">mentors@team900.org</a>".html_safe
   end
+  def title(page_title)
+    content_for(:title) { "#{page_title} - Zebracorns FRC 900" }
+  end
 end
