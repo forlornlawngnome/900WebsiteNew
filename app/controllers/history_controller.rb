34ed2dbf63feee951d3d004fc11888f0ea7d6474
Meg <forlornlawngnome@gmail.com>
2016-07-03 19:45:13 -0400

    set to nil if there is no current year

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -7,8 +7,10 @@ class HistoryController < ApplicationController
     @games = Game.joins(:year).order("start_date desc")
     if !params[:game].nil?
       @game = Game.where(:name=>params[:game]).first
-    else
+    elsif !Year.current_year.nil?
       @game = Year.current_year.game
+    else
+      @game = nil
     end
     @sponsors_by_level = @game.year.sponsor_years.group_by{ |sponsor| sponsor.sponsor_level}.sort_by{ |order| order[0].order}
     #doesn't render normal page layout
