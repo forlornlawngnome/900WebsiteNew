f7abfeddfdab783d2dbb0057c129c2ff39d15fd6
Meg <forlornlawngnome@gmail.com>
2016-07-03 19:41:07 -0400

    no sponsor levels exist

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -7,12 +7,20 @@ class Sponsor < ActiveRecord::Base
   
   def self.current_institutional_sponsors
     institutional= SponsorLevel.where(:name=>"Institutional").first
-    sponsors = Year.current_year.sponsor_years.where(:sponsor_level=>institutional)
-    sponsors.sort_by{|s| s.sponsor.name.downcase}
+    if !institutional.nil?
+      sponsors = Year.current_year.sponsor_years.where(:sponsor_level=>institutional)
+      sponsors.sort_by{|s| s.sponsor.name.downcase}
+    else
+      return nil
+    end
   end
   def self.current_gold_sponsors
     gold= SponsorLevel.where(:name=>"Gold").first
-    sponsors = Year.current_year.sponsor_years.where(:sponsor_level=>gold)
-    sponsors.sort_by{|s| s.sponsor.name.downcase}
+    if !gold.nil?
+      sponsors = Year.current_year.sponsor_years.where(:sponsor_level=>gold)
+      sponsors.sort_by{|s| s.sponsor.name.downcase}
+    else
+      return nil
+    end
   end
 end
