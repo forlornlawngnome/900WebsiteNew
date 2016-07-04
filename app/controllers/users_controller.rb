7c4e5877330a3ac091570ba2e7415d74ecfd1326
Meg <forlornlawngnome@gmail.com>
2016-07-03 21:28:27 -0400

    allow create through authentication filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_filter :authorize, :only => [:signup]
+  skip_before_filter :authorize, :only => [:signup, :create]
 
   # GET /users
   # GET /users.json
